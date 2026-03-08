67af5bd55d98501fc2d532c95225fe47858056bb
michael friedman <myklp1013@gmail.com>
2026-03-08 09:08:46 +0200

    finish the 3 step

--- load_informetion_service/kafka_publisher.py
+++ load_informetion_service/kafka_publisher.py
@@ -1,15 +1,27 @@
 from confluent_kafka import Producer
 import json
 import os
+from logs import Logger
+
 
 class KafkaPublisher:
 
     def __init__(self):
+        self.logger = Logger.get_logger(name="load_informetion/kafka_publisher")
         kafka_uri = os.getenv("KAFKA_URI","localhost:9092")
-        producer_config = {"bootstrap.servers": kafka_uri}
-        self.producer = Producer(producer_config)
+        try:
+            producer_config = {"bootstrap.servers": kafka_uri}
+            self.producer = Producer(producer_config)
+            self.logger.info("the kafka publisher connect successfully")
+        except Exception as error:
+            self.logger.error("ooooopsss kafka bublisher not connect beacuse: ",error)
+
 
     def prduce_data(self,data):
-        value = json.dumps(data).encode("utf-8")
-        self.producer.produce(topic="metadata",value=value)
-        self.producer.flush()
+        try:
+            value = json.dumps(data).encode("utf-8")
+            self.producer.produce(topic="metadata",value=value)
+            self.producer.flush()
+            self.logger.info("the data prduce successfully")
+        except Exception as error:
+            self.logger.error("ooooopsss the data not prudced beacuse: ",error)
--- load_informetion_service/main.py
+++ load_informetion_service/main.py
@@ -2,14 +2,20 @@ from pathlib import Path
 
 from extract_metadata import Metadata as md
 from kafka_publisher import KafkaPublisher
+from logs import Logger
 
-
+logger = Logger.get_logger(name="load_informetion/main")
 producer = KafkaPublisher()
 try:
-    p = Path(__file__).parent.parent / "podcasts"
+    logger.info("starting the system")
+    try:
+        p = Path(__file__).parent.parent / "podcasts"
+        logger.info("the path its corrct :)")
+    except Exception as error:
+        logger.error("the path is not correct beacose: ",error)
 
     for f in p.iterdir():
         data = md.extract_metadata(f)
         producer.prduce_data(data)
 except Exception as error:
-    print("❌ERROR:", error)
+    logger.error("the system fail beacose:",error)
